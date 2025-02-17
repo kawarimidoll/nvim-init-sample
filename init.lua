7e7e8c4b1f08297ed10feee0775178c7bcda2f26
kawarimidoll <kawarimidoll+git@gmail.com>
2025-02-17 23:23:02 +0900

    feat: add options

--- init.lua
+++ init.lua
@@ -1,6 +1,25 @@
 -- share clipboard with OS
 vim.opt.clipboard:append('unnamedplus,unnamed')
 
+-- use 2 spaces tab
+vim.opt.expandtab = true
+vim.opt.shiftround = true
+vim.opt.shiftwidth = 2
+vim.opt.softtabstop = 2
+vim.opt.tabstop = 2
+
+-- scroll before 3 lines
+vim.opt.scrolloff = 3
+
+-- move the cursor to the previous/next line across the first/last character
+vim.opt.whichwrap = 'b,s,h,l,<,>,[,],~'
+
+-- improve command line completion
+vim.opt.wildmode = { 'longest', 'full' }
+
+-- set window title
+vim.opt.title = true
+
 vim.api.nvim_create_user_command(
   'InitLua',
   function()
