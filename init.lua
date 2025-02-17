86ceef28773a39b4e2164f4c4388d311027c3a6a
kawarimidoll <kawarimidoll+git@gmail.com>
2025-02-18 08:39:14 +0900

    feat: add CopyLastCmd

--- init.lua
+++ init.lua
@@ -27,3 +27,13 @@ vim.api.nvim_create_user_command(
   end,
   { desc = 'Open init.lua' }
 )
+
+vim.api.nvim_create_user_command(
+  'CopyLastCmd',
+  function()
+    vim.fn.setreg('*', vim.fn.getreg(':'))
+    -- unless vim.opt.clipboard has unnamed
+    -- vim.fn.setreg('', vim.fn.getreg(':'))
+  end,
+  { desc = 'Copy last used command' }
+)
