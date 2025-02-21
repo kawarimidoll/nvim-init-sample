d2f8a59189e69d48d5d59a91fc774b1dcf244950
kawarimidoll <kawarimidoll+git@gmail.com>
2025-02-21 21:07:51 +0900

    feat: add display improvement modules

--- init.lua
+++ init.lua
@@ -197,3 +197,24 @@ later(function()
     },
   })
 end)
+
+later(function()
+  require('mini.cursorword').setup()
+end)
+
+later(function()
+  require('mini.indentscope').setup()
+end)
+
+later(function()
+  require('mini.trailspace').setup()
+
+  vim.api.nvim_create_user_command(
+    'Trim',
+    function()
+      MiniTrailspace.trim()
+      MiniTrailspace.trim_last_lines()
+    end,
+    { desc = 'Trim trailing space and last blank lines' }
+  )
+end)
