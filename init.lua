970154dbd4dde2e76734d76c1e5122a9166b30f0
kawarimidoll <kawarimidoll+git@gmail.com>
2025-02-21 09:03:02 +0900

    feat: setup base16 colorscheme

--- init.lua
+++ init.lua
@@ -166,3 +166,18 @@ now(function()
     ERROR = { duration = 10000 }
   })
 end)
+
+now(function()
+  local base16 = require('mini.base16')
+  local zenn_palette = base16.mini_palette(
+    '#0a2a2a', -- background
+    '#edf2f6', -- foreground
+    75         -- accent chroma
+  )
+  base16.setup({ palette = zenn_palette })
+
+  -- overwrite highlight WinSeparator
+  vim.api.nvim_set_hl(0, 'WinSeparator', { link = 'Comment' })
+  -- call autocmd ColorScheme manually
+  vim.api.nvim_exec_autocmds('ColorScheme', {})
+end)
