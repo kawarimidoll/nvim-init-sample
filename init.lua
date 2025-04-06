7fa2a0f819e9b31b3c6aee71149702f71bea8371
kawarimidoll <kawarimidoll+git@gmail.com>
2025-04-06 20:37:08 +0900

    feat: add lsp setting
    currently only for lua-language-server

--- init.lua
+++ init.lua
@@ -320,3 +320,53 @@ later(function()
     },
   })
 end)
+
+now(function()
+  vim.diagnostic.config({
+    virtual_text = true
+  })
+
+  create_autocmd('LspAttach', {
+    callback = function(args)
+      local client = assert(vim.lsp.get_client_by_id(args.data.client_id))
+
+      vim.keymap.set('n', 'grd', function()
+        vim.lsp.buf.definition()
+      end, { buffer = args.buf, desc = 'vim.lsp.buf.definition()' })
+
+      vim.keymap.set('n', '<space>i', function()
+        vim.lsp.buf.format({ bufnr = args.buf, id = client.id })
+      end, { buffer = args.buf, desc = 'Format buffer' })
+    end,
+  })
+
+  vim.lsp.config('*', {
+    root_markers = { '.git' },
+  })
+  vim.lsp.config('lua_ls', {
+    cmd = { 'lua-language-server' },
+    filetypes = { 'lua' },
+    on_init = function(client)
+      if client.workspace_folders then
+        local path = client.workspace_folders[1].name
+        if path ~= vim.fn.stdpath('config') and (vim.uv.fs_stat(path .. '/.luarc.json') or vim.uv.fs_stat(path .. '/.luarc.jsonc')) then
+          return
+        end
+      end
+      client.config.settings.Lua = vim.tbl_deep_extend('force', client.config.settings.Lua, {
+        runtime = { version = 'LuaJIT' },
+        workspace = {
+          checkThirdParty = false,
+          library = vim.list_extend(vim.api.nvim_get_runtime_file('lua', true), {
+            '${3rd}/luv/library',
+            '${3rd}/busted/library',
+          }),
+        }
+      })
+    end,
+    settings = {
+      Lua = {}
+    }
+  })
+  vim.lsp.enable('lua_ls')
+end)
