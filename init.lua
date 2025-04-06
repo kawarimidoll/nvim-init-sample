d9986d53bd866daffab4052a2c5683370724e3b8
kawarimidoll <kawarimidoll+git@gmail.com>
2025-04-06 17:05:52 +0900

    feat: add mini.hipatterns

--- init.lua
+++ init.lua
@@ -197,3 +197,19 @@ now(function()
   -- call autocmd ColorScheme manually
   vim.api.nvim_exec_autocmds('ColorScheme', {})
 end)
+
+later(function()
+  local hipatterns = require('mini.hipatterns')
+  local hi_words = require('mini.extra').gen_highlighter.words
+  hipatterns.setup({
+    highlighters = {
+      -- Highlight standalone 'FIXME', 'HACK', 'TODO', 'NOTE'
+      fixme = hi_words({ 'FIXME', 'Fixme', 'fixme' }, 'MiniHipatternsFixme'),
+      hack = hi_words({ 'HACK', 'Hack', 'hack' }, 'MiniHipatternsHack'),
+      todo = hi_words({ 'TODO', 'Todo', 'todo' }, 'MiniHipatternsTodo'),
+      note = hi_words({ 'NOTE', 'Note', 'note' }, 'MiniHipatternsNote'),
+      -- Highlight hex color strings (`#rrggbb`) using that color
+      hex_color = hipatterns.gen_highlighter.hex_color(),
+    },
+  })
+end)
