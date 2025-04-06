48f35c46d538e99280becc59c359d859dd986d73
kawarimidoll <kawarimidoll+git@gmail.com>
2025-04-06 09:46:49 +0900

    feat: add autocmds

--- init.lua
+++ init.lua
@@ -21,3 +21,27 @@ vim.api.nvim_create_user_command(
   end,
   { desc = 'Open init.lua' }
 )
+
+-- augroup for this config file
+local augroup = vim.api.nvim_create_augroup('init.lua', {})
+
+-- wrapper function to use internal augroup
+local function create_autocmd(event, opts)
+  vim.api.nvim_create_autocmd(event, vim.tbl_extend('force', {
+    group = augroup,
+  }, opts))
+end
+
+-- https://vim-jp.org/vim-users-jp/2011/02/20/Hack-202.html
+create_autocmd('BufWritePre', {
+  pattern = '*',
+  callback = function(event)
+    local dir = vim.fs.dirname(event.file)
+    local force = vim.v.cmdbang == 1
+    if vim.fn.isdirectory(dir) == 0
+        and (force or vim.fn.confirm('"' .. dir .. '" does not exist. Create?', "&Yes\n&No") == 1) then
+      vim.fn.mkdir(vim.fn.iconv(dir, vim.opt.encoding:get(), vim.opt.termencoding:get()), 'p')
+    end
+  end,
+  desc = 'Auto mkdir to save file'
+})
