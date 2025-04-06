cfb34652e27b38055a0b4ce17b188217bf0ff3ec
kawarimidoll <kawarimidoll+git@gmail.com>
2025-04-06 16:23:55 +0900

    feat: add mini.misc

--- init.lua
+++ init.lua
@@ -161,3 +161,14 @@ now(function()
     end,
   })
 end)
+
+now(function()
+  require('mini.misc').setup()
+
+  MiniMisc.setup_restore_cursor()
+
+  vim.api.nvim_create_user_command('Zoom', function()
+    MiniMisc.zoom(0, {})
+  end, { desc = 'Zoom current buffer' })
+  vim.keymap.set('n', 'mz', '<cmd>Zoom<cr>', { desc = 'Zoom current buffer' })
+end)
