509f1296b140c1bcb66fd5d737299afeb497358b
kawarimidoll <kawarimidoll+git@gmail.com>
2025-04-06 17:48:45 +0900

    feat: add pair-improvement modules

--- init.lua
+++ init.lua
@@ -238,3 +238,25 @@ end)
 now(function()
   require('mini.starter').setup()
 end)
+
+later(function()
+  require('mini.pairs').setup()
+end)
+
+later(function()
+  require('mini.surround').setup()
+end)
+
+later(function()
+  local gen_ai_spec = require('mini.extra').gen_ai_spec
+  require('mini.ai').setup({
+    custom_textobjects = {
+      B = gen_ai_spec.buffer(),
+      D = gen_ai_spec.diagnostic(),
+      I = gen_ai_spec.indent(),
+      L = gen_ai_spec.line(),
+      N = gen_ai_spec.number(),
+      J = { { '()%d%d%d%d%-%d%d%-%d%d()', '()%d%d%d%d%/%d%d%/%d%d()' } }
+    },
+  })
+end)
